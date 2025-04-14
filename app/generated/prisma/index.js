351a9cd10d0e03d3ddc498569dcc6583d54cb3ed
Karl Litterfeldt <kalle.litterfeldt@gmail.com>
2025-04-14 10:23:25 +0200

    Added prisma generate to build stage to allow for Vercel Caching workaround

--- app/generated/prisma/index.js
+++ app/generated/prisma/index.js
@@ -158,11 +158,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://username:password@localhost:5432/puppy_spa"
+        "value": null
       }
     }
   },
